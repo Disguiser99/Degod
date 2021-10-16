6ee93f0c1cb1d30526e908baf73fb7cc2aa17b68
Disguiser <wangyong01@rd.netease.com>
2021-10-16 13:37:33 +0800

    ADD：demo 注释

--- demo/src/main/java/com/disguiser/app/MainActivity.kt
+++ demo/src/main/java/com/disguiser/app/MainActivity.kt
@@ -23,11 +23,16 @@ class MainActivity : AppCompatActivity() {
         val testString = "testStringEntry"
         val testString2 = "testStringEntry2"
         val testString3 = "testStringEntry3, %s"
+        // 直接打印字符串
         Degod.eM("start record")
-        Degod.tag(testTag).e("start record with testTag")
-        Degod.e(testString, testString2, testString3)
+        // 打印字符串与参数格式化后的结果
         Degod.eM("start record %s", hello, world)
         Degod.eM("start record %s %s", hello, world)
+        // 设置自定义tag（仅被使用一次）
+        Degod.tag(testTag).e("start record with testTag")
+        // 打印传入的可变参数的字符串形式
+        Degod.e(testString, testString2, testString3)
+        // 设置是否能被打印
         Degod.setLoggable(false)
         Degod.e("start record after unLoggable")
     }
